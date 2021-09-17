c38fba3713b38a360e9acf3132dcf034f8da48d0
sam <swzwij@gmail.com>
2021-09-17 10:20:12 +0200

    les 2

---
+++ c#les2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemy myEnemy = new Enemy();
+
+            while (myEnemy.isDead == false)
+            {
+                Console.WriteLine("hit the Enemy? type 'hit'");
+                string awnser = Console.ReadLine();
+
+                if (awnser == "hit")
+                {
+                    myEnemy.damage();
+                }
+
+            }
+
+        }
+    }
+
+    class Enemy : Program
+    {
+        public bool isDead = false;
+        bool takenHit = true;
+        int Health = 10;
+
+
+        public void damage()
+        {
+            if (takenHit == true)
+            {
+                Health = Health - 1;
+                Console.WriteLine("Enemy health is " + Health);
+            }
+
+            if (Health == 0)
+            {
+                isDead = true;
+                Console.WriteLine("Enemy is dead");
+            }
+
+        }
+    }
+}
